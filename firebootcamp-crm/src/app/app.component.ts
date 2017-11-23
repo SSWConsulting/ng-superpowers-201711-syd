b12af5a25c5538a2fca09367931315e39cbd48d3
brendan richards <brendan@nobadthing.com>
2017-11-24 09:14:35 +1000

    added hello world

---
+++ firebootcamp-crm/src/app/app.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'fbc-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'hello Sydney';
+
+  onChange() {
+    console.log('CHANGED!!');
+  }
+}
